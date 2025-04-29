ec2af51dfb0ac019b67934584716f76e07d6e5db
Adarsh Rathore <adarshrathore380@gmail.com>
2025-04-29 22:40:43 +0530

    tracker added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // <-- import BrowserRouter
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { PostHogProvider } from 'posthog-js/react';
+import './index.css';
+import App from './App.jsx';
+
+const apiKey = "phc_MYyQVyOSm4DrfFzKjXBTw04izMFFujeLW9v6H1Sb5mF";
+const apiHost = `https://us.i.posthog.com`;
+;
+
+
+console.log('PostHog KEY:', apiKey);
+console.log('PostHog HOST:', apiHost);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> 
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <PostHogProvider
+      apiKey={apiKey}
+      options={{ api_host: apiHost }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PostHogProvider>
+  </StrictMode>
+);
